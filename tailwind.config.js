579de572ac3db00a64c7b10f90795cba065b6ce7
Jon Fleming <jonfleming@hotmail.com>
2025-05-15 15:54:50 -0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,12 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    'bg-green-100',
+    'text-green-700',
+    'border-green-200',
+    'bg-red-100',
+    'text-red-700',
+    'border-red-200',
+  ]
 }
